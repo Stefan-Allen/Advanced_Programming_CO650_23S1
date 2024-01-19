7eab732d6e9198aabc6a36302de2246da6609743
Stefan-Allen <Stefanallen10@gmail.com>
2024-01-19 01:37:59 +0000

    Added comments to exeption

--- Client/src/Headers/Exception.h
+++ Client/src/Headers/Exception.h
@@ -6,8 +6,10 @@
 
 class Exception : public std::exception {
 public:
+    // Constructors
     Exception(const std::string& message, int errorCode = 0) : message(message), errorCode(errorCode) {}
 
+    // Method to provide detailed error information
     const char* what() const noexcept override {
         if (errorCode != 0) {
             return (message + " Error Code: " + std::to_string(errorCode)).c_str();
@@ -16,11 +18,13 @@ public:
         }
     }
 
+    // Getter for the error code
     int GetErrorCode() const {
         return errorCode;
     }
 
 private:
+    // Private members
     std::string message;
     int errorCode;
 };
